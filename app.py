7adb1a594e12a49c5e4ae6f444ebbb4fe44844b9
Lalbabu <lalbabusah418@gmail.com>
2021-08-19 23:20:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,37 @@
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, request, jsonify
 from flask_restful import Resource, Api
+from elasticsearch import Elasticsearch
 import requests
+import json
 
 
 app = Flask(__name__)
-api = Api(app)
+es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
 
 
-class get_all_articles(Resource):
-    def get(self, keyword='Space'):
-        api_url = 'https://api.spaceflightnewsapi.net/v3/articles'
-        response = requests.get(api_url)
-        responses = response.json()
-
-        articles = list()
-        for article in responses:
-            if keyword in article['title']:
-                articles.append(article['title'])
-
-        return articles
+def store_record():
+    url = 'https://api.spaceflightnewsapi.net/v3/articles'
+    response = requests.get(url)
+    responses = response.json()
+    for response in responses:
+        es.index(index='myindex', doc_type='article', id=response['id'], body=response)
+    return None
 
 
-api.add_resource(get_all_articles, '/')
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+@app.route('/search', methods=['GET'])
+def article_search():
+    keyword = 'Space' #request.args.get('keyword')
+    keyword_search = json.dumps({'query': {'match': {'title': keyword}}})
+    resources = es.search(index='myindex', body=keyword_search)
+
+    articles = list()
+    for article in resources['hits']['hits']:
+        articles.append(article['_source']['title'])
+    print(articles)
+    return articles
+
+
+if __name__ == '__main__':
+    store_record()
+    article_search()
+    #app.run(host='0.0.0.0', port=5000)
