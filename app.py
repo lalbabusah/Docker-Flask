35e5b148b7eab884e19d56b69e82f0cb8037e82a
Lalbabu <lalbabusah418@gmail.com>
2021-08-20 14:34:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,28 +10,46 @@ es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
 
 
 def store_record():
-    url = 'https://api.spaceflightnewsapi.net/v3/articles'
-    response = requests.get(url)
-    responses = response.json()
-    for response in responses:
-        es.index(index='myindex', doc_type='article', id=response['id'], body=response)
-    return None
+    try:
+        url = 'https://api.spaceflightnewsapi.net/v3/articles'
+        response = requests.get(url)
+        responses = response.json()
+        for response in responses:
+            es.index(index='myindex', doc_type='article', id=response['id'], body=response)
+        print('Data stored')
+
+    except:
+        print('Some Error Occurred')
+        return None
 
 
 @app.route('/search', methods=['GET'])
 def article_search():
-    keyword = 'Space' #request.args.get('keyword')
-    keyword_search = json.dumps({'query': {'match': {'title': keyword}}})
-    resources = es.search(index='myindex', body=keyword_search)
+    keyword = request.args.get("keyword")
 
-    articles = list()
-    for article in resources['hits']['hits']:
-        articles.append(article['_source']['title'])
-    print(articles)
-    return articles
+    try:
+        keyword_search = json.dumps({'query': {'match': {'title': keyword}}})
+        resources = es.search(index='myindex', body=keyword_search)
+
+        articles = list()
+        for article in resources['hits']['hits']:
+            articles.append(article['_source']['title'])
+        #print(articles)
+        response = jsonify(articles=articles), 200
+
+    except:
+        response = jsonify(message='Some Error Occurred'), 404
+
+    finally:
+        return response
 
 
 if __name__ == '__main__':
-    store_record()
-    article_search()
-    #app.run(host='0.0.0.0', port=5000)
+    if es.ping():
+        print('Connected')
+        store_record()
+        app.run(host='0.0.0.0', port=5000)
+    else:
+        print('Not Connected')
+
+
